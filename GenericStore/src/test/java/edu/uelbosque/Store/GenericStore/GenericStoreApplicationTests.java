fceb7a610bcb31ec502cb0ff1240a409161a0c19
Andrés Torres <anfeto334@gmail.com>
2021-11-17 13:12:28 -0500

    Inicial

---
+++ GenericStore/src/test/java/edu/uelbosque/Store/GenericStore/GenericStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uelbosque.Store.GenericStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
